221072f02616629bf05520271d6e9ec68b1aeec9
willian-pessoa <willianpessoadeabreu@gmail.com>
2024-01-18 12:35:17 -0300

    feat: identifica todos os elementos por css seletor

--- js/behaviorTracker.js
+++ js/behaviorTracker.js
@@ -1,17 +1,59 @@
-const normalizeFPS = callback => {
-  let ticking = true;
-  const update = () => {
-    if (ticking) requestAnimationFrame(update);
-    ticking = true;
-  };
-  return event => {
-    if (ticking) {
-      callback(event);
-      update();
+const GLOBAL_MOUSE_DATA = {}
+
+const getSiblingIndex = (element) => {
+  if (!element || !element.parentElement?.children) return 0
+
+  let siblingIndex = 0
+
+
+  for (let item of element.parentElement.children) {
+    if (item.__heatmap_node_map_id__ === element.__heatmap_node_map_id__) {
+      return siblingIndex
     }
-    ticking = false;
-  };
-};
+
+    siblingIndex++
+  }
+
+  return siblingIndex
+}
+
+const getNodeName = (element) => {
+  if (!element) return
+
+  let nodeName = element.nodeName.toLowerCase()
+  let siblingIndex = getSiblingIndex(element)
+
+  return !siblingIndex ? nodeName : `${nodeName}[${siblingIndex + 1}]`
+}
+
+const getParentsElements = (element) => {
+  if (!element) return
+
+  if (element.parentElement.nodeName === "BODY") {
+    return "body"
+  }
+
+  if (element.parentElement) {
+    let parentNodeName = " > " + getNodeName(element.parentElement)
+
+    return getParentsElements(element.parentElement) + parentNodeName
+  } else {
+    return " > "
+  }
+}
+
+const getElementCssSelector = (element) => {
+  if (!element) return
+
+  let idCssSelector = ""
+
+  let nodeName = getNodeName(element)
+  let parents = getParentsElements(element)
+
+  idCssSelector = parents + " > " + nodeName
+
+  return idCssSelector
+}
 
 const trackMouse = () => {
   const mouseTrack = {
@@ -27,31 +69,36 @@ const trackMouse = () => {
   const data = []
 
   // Envia as coordenadas X / Y, Timestamp e tipo de evento
-  const pushEventData = ({ pageX, pageY, type, clientX, clientY, target }) => {
+  const pushEventData = (event) => {
+    const { type } = event
     if (type === "click") {
-      let TotalX = document.body.scrollWidth
-      let TotalY = document.body.scrollHeight
-      let posX = (pageX * 100) / TotalX
-      let posY = (pageY * 100) / TotalY
-
-      console.log("Pos: " + posX + "," + posY)
-      console.log("Page: " + pageX + "," + pageY)
-
+      const seletor = event
+      console.log(seletor)
       data.push({
         time: Date.now(),
-        pageX, //position relative to page screen
-        pageY,
-        clientX, //
-        clientY,
-        posX,
-        posY,
-        type,
       });
     }
+
+    event.stopPropagation()
   };
 
-  //document.addEventListener("mousemove", normalizeFPS(pushEventData));
-  document.addEventListener("click", pushEventData);
+  const body = document.querySelector("body");
+  const elements = body.querySelectorAll("*");
+
+  let countId = 1
+
+  elements.forEach((element) => {
+    element.addEventListener("click", pushEventData)
+    element["__heatmap_node_map_id__"] = countId
+
+    GLOBAL_MOUSE_DATA[countId] = {
+      idCssSelector: getElementCssSelector(element),
+      clicks: 0,
+      data: [],
+    }
+
+    countId++
+  });
 
   mouseTrack.data = data
 
